3c6df7f98657cd5c7ae54f8031208a3492ffd8fa
kislaytt <98452481+kislaytt@users.noreply.github.com>
2023-12-14 02:21:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-
-from flask import Flask
-
+from flask import Flask, render_template, Response
+import cv2
 
 app = Flask(__name__)
 
+def generate_frames():
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+
+        # Your image processing logic here (e.g., drawing lines)
+        frame = cv2.line(frame, (0, 0), (frame.shape[1], frame.shape[0]), (0, 255, 0), 2)
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame = jpeg.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+    camera.release()
+
 @app.route('/')
+def index():
+    return render_template('index.html')
 
-def hello_world():
-	return 'Hello World'
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# main driver function
 if __name__ == '__main__':
-
-	
-	app.run()
+    app.run(debug=True)
